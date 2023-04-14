d788f795e65bec64e70ec84fd4dd6ea83c932cea
MatiasCV27 <matiascriollo57@gmail.com>
2023-04-14 18:31:01 -0500

    Cosas

--- RegistroArtesania_IRS/FrmRegistrarArtesania.cs
+++ RegistroArtesania_IRS/FrmRegistrarArtesania.cs
@@ -19,9 +19,6 @@ namespace RegistroArtesania_IRS
             AgregarEmpleados();
         }
 
-        // PRUEBA Y ERROR XD
-
-
         SqlConnection Con = new SqlConnection("User Id=sa;Password=12345;Initial Catalog=IRS1;Data Source=(local);");
 
         objRegistrarArtesania.clsArtesania Art = new objRegistrarArtesania.clsArtesania();
